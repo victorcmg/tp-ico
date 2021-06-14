cd8c6e83dfc9e7b746203bc0ff2500a7b292fae1
santosba <gustavoba8@gmail.com>
2021-06-14 20:40:02 +0100

    json-concluido

--- VRP/static/js/app.js
+++ VRP/static/js/app.js
@@ -81,39 +81,6 @@ function address_search()
  xmlhttp.send();
 }
 
- //document.querySelector('.btn-sender').addEventListener('click',sendJson)
-
-
-
- /*function sendJson(){
-   console.log(coord)
-  let xhr = new XMLHttpRequest();
-  let url = "post/request/ajax";
-//{% url 'index' %}
-  // open a connection
-  xhr.open("POST", url, true);
-
-  // Set the request header i.e. which type of content you are sending
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  // Create a state change callback
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-
-          // Print received data from server
-         // result.innerHTML = this.responseText;
-
-      }
-  };
-
-  // Converting JSON data to string
-  var data = JSON.stringify({ "post_data":coord});
-
-  
-  // Sending data with the request
-  xhr.send(data);
-}*/
-
 
 $(".btn-sender").click(function () {
 console.log(coord)
--- VRP/views.py
+++ VRP/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import TemplateView , ListView
 import json 
+import os
 from . models import Problem
 from VRP.routing.vrp import *
 from django.views.decorators.csrf import csrf_protect
@@ -53,36 +54,56 @@ def ajax_post_view(request):
         str_of_ints = ",".join(string_ints)
         routes_str.append(str_of_ints)
         
-    data = {
-        "ID": "1",
-        "Algorithm": "a",
-        "Vehicles": len(routes),
-        "Routes":routes_str,
-        "Distance":distances
-    }
-   
-    print(data)
+    
+    #print(data)
     #print(distances)
     #json_object = json.dumps(new_data, indent = 6)
   
 # Serializing json 
     #json_object = json.dumps(data)
-  
+       
 # Writing to sample.json
     #with open("VRP/static/images/data.json", "w") as outfile:
         #outfile.write(json_object)
-    #write_json(data,'VRP/static/images/data.json')    
+    #object_data=[]
+    #with open('VRP/static/images/data.json')  as json_file:
+    #object_data = json.load(json_file)
+        
+        new_data = {
+        "ID": "1",
+        "Algorithm": "a",
+        "Vehicles": len(routes),
+        "Routes":routes_str,
+        "Distance":distances
+        }
+        #object_data.append(new_data)
+    write_json(new_data,'VRP/static/images/data.json')    
     
     return JsonResponse({'data': 'data'})
 
-
-def write_json(new_data, filename='data.json'):
-    with open(filename,'r+') as file:
-          # First we load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_dat3a with file_data
-        file_data.update(new_data)
-        # Sets file's current position at offset.
-        #file.seek(0)
+lista=[]
+def write_json(data,filename):
+    if os.stat(filename).st_size ==0:
+        with open(filename,mode='w') as f:
+            lista.append(data)
+            json.dump(lista,f,indent=4)       
+    else:
+        
+         # ler o ficheiro
+        with open(filename,'r') as file:
+            dados = json.load(file)
+            # actualizar
+            dados.append(data)
+        #with open(filename,'w') as file:
+            #json.dump(data,file, indent=4)
+            file.seek(0)
+       
+        with open(filename,"w") as file:    
         # convert back to json.
-        json.dump(file_data, file, indent = 6)
+            json.dump(dados, file, indent = 4)
+             
+        #file_data = json.load(file)
+        # Join new_dat3a with file_data
+        #file_data.update(new_data)
+        # Sets file's current position at offset.
+        
